f3c15dd46b28679ee51beeed3d63fc05e5eee4eb
t10471 <t104711202@gmail.com>
2022-08-23 19:37:41 +0900

    add stream

--- grpc-examples/go/cmd/main.go
+++ grpc-examples/go/cmd/main.go
@@ -13,56 +13,56 @@ func main() {
 	app := &cli.App{
 		Commands: []*cli.Command{
 			{
-				Name:    "client",
+				Name: "client",
 				Flags: []cli.Flag{
 					&cli.StringFlag{
-						Name:    "address",
-						Value:   "localhost:50051",
+						Name:  "address",
+						Value: "localhost:50051",
 					},
 				},
-				Action:  func(c *cli.Context) error {
+				Action: func(c *cli.Context) error {
 					return client.ClientMain(
 						c.String("address"),
 					)
 				},
 			},
 			{
-				Name:    "hw-simple",
+				Name: "hw-simple",
 				Flags: []cli.Flag{
 					&cli.StringFlag{
-						Name:    "my-address",
-						Value:   "localhost:50051",
+						Name:  "my-address",
+						Value: "localhost:50051",
 					},
 					&cli.StringFlag{
-						Name:    "other-address",
-						Value:   "localhost:50052",
+						Name:  "other-address",
+						Value: "localhost:50052",
 					},
 					&cli.BoolFlag{
-						Name:    "unwrap-error",
-						Value:   true,
+						Name:  "unwrap-error",
+						Value: true,
 					},
 				},
-				Action:  func(c *cli.Context) error {
+				Action: func(c *cli.Context) error {
 					return server.HWSimpleMain(
 						c.String("my-address"),
 						c.String("other-address"),
 						c.Bool("unwrap-error"),
-						)
+					)
 				},
 			},
 			{
-				Name:    "hw-complicated",
+				Name: "hw-complicated",
 				Flags: []cli.Flag{
 					&cli.StringFlag{
-						Name:    "my-address",
-						Value:   "localhost:50053",
+						Name:  "my-address",
+						Value: "localhost:50053",
 					},
 					&cli.StringFlag{
-						Name:    "other-address",
-						Value:   "localhost:50052",
+						Name:  "other-address",
+						Value: "localhost:50052",
 					},
 				},
-				Action:  func(c *cli.Context) error {
+				Action: func(c *cli.Context) error {
 					return server.HWComplicatedMain(
 						c.String("my-address"),
 						c.String("other-address"),
@@ -70,21 +70,54 @@ func main() {
 				},
 			},
 			{
-				Name:    "other",
+				Name: "other",
 				Flags: []cli.Flag{
 					&cli.StringFlag{
-						Name:    "address",
-						Value:   "localhost:50053",
+						Name:  "address",
+						Value: "localhost:50053",
 					},
 					&cli.IntFlag{
-						Name:    "sleep",
-						Value:   3,
+						Name:  "sleep",
+						Value: 3,
 					},
 				},
-				Action:  func(c *cli.Context) error {
+				Action: func(c *cli.Context) error {
 					return server.OtherMain(
-						c.String("my-address"),
+						c.String("address"),
 						c.Int("sleep"),
+					)
+				},
+			},
+			{
+				Name: "stream",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:  "address",
+						Value: "localhost:50054",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					return server.StreamMain(
+						c.String("address"),
+					)
+				},
+			},
+			{
+				Name: "stream-client",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:  "address",
+						Value: "localhost:50054",
+					},
+					&cli.StringFlag{
+						Name:  "method",
+						Value: "server",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					return client.StreamMain(
+						c.String("address"),
+						c.String("method"),
 					)
 				},
 			},
